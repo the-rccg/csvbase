6ca80a9caaa44fd44881829b2ce7a13a20df9f60
Robin <r.greif@outlook.com>
2020-11-22 16:25:28 +0100

    black formatting and docstrings

--- data_server.py
+++ data_server.py
@@ -9,15 +9,20 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def home():
+    """provide the homedirectory index to browse"""
     output = f"<h1>home data directory</h1>"
-    output += str(ulify([make_url(request.base_url, f) for f in get_contents(os.getcwd())]))  # str(get_hierarchy(os.getcwd()))
+    output += str(
+        ulify([make_url(request.base_url, f) for f in get_contents(os.getcwd())])
+    )  # str(get_hierarchy(os.getcwd()))
     return output
 
-@app.route('/', defaults={'req_path': ''})
-@app.route('/<path:req_path>')
+
+@app.route("/", defaults={"req_path": ""})
+@app.route("/<path:req_path>")
 def dir_listing(req_path):
+    """dynamic routing to allow the exploration of subdirectories"""
     # https://stackoverflow.com/questions/23718236/python-flask-browsing-through-directory-with-files
 
     # Joining the base and the requested path
@@ -33,16 +38,20 @@ def dir_listing(req_path):
 
     # Show directory contents
     output = f"<h1>{req_path}</h1>"
-    output += str(ulify([make_url(request.base_url, f) for f in get_contents(abs_path)]))
+    output += str(
+        ulify([make_url(request.base_url, f) for f in get_contents(abs_path)])
+    )
     return output  # render_template('files.html', files=files)
 
-@app.route('/SQL', defaults={'query': ''}, methods=['GET'])
-def sql(query=''):
-    if 'query' in request.args:
-        query = str(request.args['query'])
+
+@app.route("/SQL", defaults={"query": ""}, methods=["GET"])
+def sql(query=""):
+    """provide a SQL endpoint to query the files"""
+    if "query" in request.args:
+        query = str(request.args["query"])
     else:
         return "Error: No query provided. Please specify a SQL Query."
-    
+
     # Example "csvsql --query \"select Open from 'AAPL' where Open > 400\" Marketdata/YahooFinance/AAPL.csv"
     data = subprocess.check_output(f"csvsql --query {query}", shell=True)
 
--- data_tools.py
+++ data_tools.py
@@ -1,8 +1,12 @@
 import os
 
+
 BASE_PATH = ""
 
+
 def get_hierarchy(path):
+    """return a nested hierarchy for a directory including dictionaries of subdirectories and lists of files"""
+
     def get_contents(path, include_files=True):
         h = []
         p = {}
@@ -17,10 +21,13 @@ def get_hierarchy(path):
         if len(p):
             h.append(p)
         return h
+
     hierarchy = get_contents(path)
     return hierarchy
 
+
 def get_contents(path):
+    """give filtered contents of a path"""
     cont = os.listdir(path)
     cont = [i for i in cont if "__" not in i]
     cont = [i for i in cont if "." not in i] + sorted([i for i in cont if "." in i])
@@ -28,10 +35,13 @@ def get_contents(path):
 
 
 def ulify(elements):
+    """create HTML of an unordered list from an iterable"""
     string = "<ul>\n"
     string += "\n".join(["<li>" + str(s) + "</li>" for s in elements])
     string += "\n</ul>"
     return string
 
+
 def make_url(base_url, element):
+    """create HTML link to an element given the base url"""
     return f"<a href={base_url.rstrip('/')}/{element}>{element}</a>"
